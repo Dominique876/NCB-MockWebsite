c042d1eba126521b43eaa36f68b11c6d4496a17f
Dominique Nelson <55509460+Dominique876@users.noreply.github.com>
2021-07-30 12:30:56 -0500

    Add files via upload

---
+++ NCBWcfService/INCBService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace NCBWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "INCBService" in both code and config file together.
+    [ServiceContract]
+    public interface INCBService
+    {
+        [OperationContract]
+        string Message();
+        [OperationContract]
+        bool Verify(string name, string cardNum);
+        [OperationContract]
+        bool VerifyFunds(float amount, string cardNum);
+        [OperationContract]
+        double CusFunds(string cardNum, double amnt);
+    }
+
+    [DataContract]
+    public class getCardData
+    {
+        [DataMember]
+        public DataTable Customer
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ NCBWcfService/NCBService.svc.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace NCBWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class NCBService : INCBService
+    {
+        public string Message()
+        {
+            return "Hello World, this is my Second WCF web service. ";
+        }
+
+        public bool Verify(string name, string cardNum)
+        {
+            getCardData g = new getCardData();
+            DataSet ds = new DataSet();
+            SqlConnection con = new SqlConnection("Data Source=(localdb)\\mssqllocaldb;Database=NCB1Context-46f8183e-abbc-4afc-bcd5-61a7874dc080;Trusted_Connection=True;MultipleActiveResultSets=true");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select Name,CardNum from Customer where CardNum ='" + cardNum + "' and First ='" + name + "' ", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            cmd.ExecuteNonQuery();
+            //g.ScotiaCustomer = dt;
+            if (dt.Rows.Count > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool VerifyFunds(float amount, string cardNum)
+        {
+            SqlConnection con = new SqlConnection("Data Source=(localdb)\\mssqllocaldb;Database=NCB1Context-46f8183e-abbc-4afc-bcd5-61a7874dc080;Trusted_Connection=True;MultipleActiveResultSets=true");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select Balance from Customer where CardNum='" + cardNum + "'", con);
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            //DataTable table = new DataTable();
+            //table.Columns.Add("Available");
+           // double ava = 0.0;
+            double bal = 0.0;
+            while (rdr.Read())
+            {
+
+                bal = Convert.ToDouble(rdr["Balance"]);
+            }
+
+
+
+            if (bal > amount)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public double CusFunds(string cardNum, double amnt)
+        {
+            SqlConnection con = new SqlConnection("Data Source=(localdb)\\mssqllocaldb;Database=NCB1Context-46f8183e-abbc-4afc-bcd5-61a7874dc080;Trusted_Connection=True;MultipleActiveResultSets=true");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Update Customer set  Balance=Balance-" + amnt + " where CardNum='" + cardNum + "'", con);
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            //DataTable table = new DataTable();
+            //table.Columns.Add("Available");
+            //double ava = 0.0;
+            double bal = 0.0;
+            while (rdr.Read())
+            {
+
+                bal = Convert.ToDouble(rdr["Balance"]);
+            }
+
+            return bal;
+
+        }
+    }
+}
